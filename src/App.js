ec88e2631037c25f4bf21ef0376388caf5611aec
Firtch <firtch@gmail.com>
2021-07-08 20:55:58 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import content from "./static";
+import { useForm } from "react-hook-form";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  username: yup.string().required("Пожалуйста введите имя"),
+  email: yup.string().required("Вы забыли ввести Email").email(),
+  password: yup.string().required("Без пароля, нету входа!").min(5, "Пароль должен содержать минимум 5 символов"),
+});
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const onSubmit = (data) => console.log(data);
+
+  console.log(errors);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React-hook-form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {content.inputs.map((input, key) => {
+          return (
+            <div key={key}>
+              <p>
+                <label className="label">{input.label}</label>
+              </p>
+              <p>
+                <input
+                  defaultValue=""
+                  type={input.type}
+                  name={input.name}
+                  className="input"
+                  {...register(input.name)}
+                />
+              </p>
+              <p className="messages">{errors[input.name]?.message}</p>
+            </div>
+          );
+        })}
+        <button className="btn" type="submit">
+          submit
+        </button>
+      </form>
     </div>
   );
 }
